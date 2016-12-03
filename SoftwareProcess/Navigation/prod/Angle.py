cb5d022bc2734671c3bc62903d4454066cbb1378
M.Lloyd <mpl0010@auburn.edu>
2016-12-02 20:05:00 -0600

    Final commit CA05

--- SoftwareProcess/Navigation/prod/Angle.py
+++ SoftwareProcess/Navigation/prod/Angle.py
@@ -5,49 +5,47 @@
     @author: M. Lloyd
 '''
 
+from decimal import *
+
 class Angle():
     def __init__(self):
         #self.angle = ...       set to 0 degrees 0 minutes
         #creates an instance of Angle.
         self.angle = 0.0
     
-    def setDegrees(self, degrees = 0):
+    def setDegrees(self, degrees = 0.0):
         #returns the resulting angle as degrees
         try:
-            self.angle = self.angle + (float(degrees) % 360)
+            self.angle = (float(degrees) % 360.0)
+            degree = int(self.angle)
+            minutes = round((self.angle - degree)*60, 1)
+            self.angle = degree + minutes/60.0
             return self.angle
         except:
             raise ValueError("Angle.setDegrees: degrees violates the parameter specifications")
     
     def setDegreesAndMinutes(self, degrees):
-        if(isinstance(degrees, basestring)):
-            strDelimiter = degrees.find("d")
-            strDecimalLength = degrees.find(".")
-            firstHalf = degrees[0:strDelimiter]
-            secondHalf = degrees[strDelimiter + 1: len(degrees)]
-            decimalPlacesAmount = degrees[strDecimalLength + 1: len(degrees)]
-            if(strDelimiter == -1):
-                raise ValueError("Angle.setDegreesAndMinutes: The value must resemble this structure (degree)d(Minutes)")
-            elif((strDecimalLength != -1) & (len(decimalPlacesAmount) != 1)):
+        try:
+            angStr = degrees.split("d")
+            if (float(angStr[1]) < 0.0):
                 raise ValueError("Angle.setDegreesAndMinutes: The Minutes value can only have one decimal place")
-            else:
-                try:
-                    if(float(secondHalf) < 0.0):
-                        raise ValueError("Angle.setDegreesAndMinutes: The Minute value must be positive")
-                    else:
-                        tempFirstHalf = int(firstHalf) % 360
-                        minuteConvert = float(secondHalf) / 60.0
-                        self.angle = tempFirstHalf + minuteConvert
-                        return self.angle
-            
-                except:
-                #handle exception
-                    raise ValueError("Angle.setDegreesAndMinutes: The value must resemble this structure (degree)d(Minutes)")
-                    
-            #Correcting the degree if greater than 360 or less than 0
-        else:
-            raise ValueError("Angle.setDegreesAndMinutes: The value needs to be an instance of a string.")
     
+            decimalCheck = abs(Decimal(angStr[1]).as_tuple().exponent)
+            if (decimalCheck > 1):
+                raise ValueError("Angle.setDegreesAndMinutes: The Minutes value can only have one decimal place")
+            
+            firstHalf = int(angStr[0])
+            secondHalf = float(angStr[1]) / 60.0
+            
+            if (-360 < firstHalf < 0):
+                self.angle = 360 - (abs(firstHalf) + secondHalf)
+            else:
+                self.angle = (firstHalf % 360) + secondHalf
+                
+            return self.angle
+        except:
+            raise ValueError("Angle.setDegreesAndMinutes: The Minutes value can only have one decimal place")
+        
     def add(self, angle = None):
         if(angle == None):
             raise ValueError("Angle.add: An angle value must be passed in")
@@ -82,14 +80,11 @@ class Angle():
             raise ValueError("Angle.compare: angle is not a valid instance of Angle")
     
     def getString(self):
-        strAngle = str(self.angle)
-        degree = strAngle[0:strAngle.find(".")]
-        degreeConverted = int(degree) % 360
-        minutes = strAngle[strAngle.find(".") + 1: len(strAngle)]
-        print minutes
-        minutesConverted = float(minutes) * 6
-        return str(degreeConverted) + "d" + str(minutesConverted)
+        strAngleDeg = str(int(self.angle) // 1)
+        strAngleMin = str(round(((self.angle % 1) * 60.0), 1))
+        return strAngleDeg + "d" + strAngleMin
     
     def getDegrees(self):
         #Returns the angle
+        self.angle = self.angle % 360
         return self.angle 
--- SoftwareProcess/Navigation/prod/Fix.py
+++ SoftwareProcess/Navigation/prod/Fix.py
@@ -1,18 +1,21 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from xml.dom import minidom
 import math
-from cmath import sqrt
+from decimal import *
 import Angle
-from math import tan
+from math import tan, sqrt, pi, sin
 import os
 
 class Fix():
     def __init__(self, logFile = "log.txt"):
         try:
             self.log = logFile
+            self.sightingFile = None
             self.strOfSightingFile = None
             self.starDictionary = None
             self.ariesDictionary = None
+            self.ariesFile = None
+            self.starFile = None
             f = open(self.log, "a")
             f.write("LOG: " + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + "-6:00\t" + "Log file:\t" + os.path.abspath(self.log) + "\n")
         except:
@@ -40,77 +43,81 @@ class Fix():
                 self.unedittedString = s.read()
                 self.strOfSightingFile = self.unedittedString.translate(None, "\t")
                 s.close()
-                return self.sightingFile
+                return os.path.abspath(self.sightingFile)
             except:    
                 raise ValueError("Fix.setSightingFile: The file violates the parameter rules")                    
         else:
             raise ValueError("Fix.setSightingFile: The file violates the parameter rules")                    
              
-    def setAriesFile(self, ariesFile):
+    def setAriesFile(self, ariesFile = None):
         if(ariesFile == None):
             raise ValueError("Fix.setAriesFile: The file violates the parameter rules")                    
-        
-        checkForPeriod = ariesFile.find(".")
-        if(checkForPeriod != -1):
-            enteredFileType = ariesFile[checkForPeriod + 1: len(ariesFile)]
-            if(enteredFileType != "txt"):
+        if(isinstance(ariesFile, basestring)):
+            checkForPeriod = ariesFile.find(".")
+            if(checkForPeriod != -1):
+                enteredFileType = ariesFile[checkForPeriod + 1: len(ariesFile)]
+                if(enteredFileType != "txt"):
+                    raise ValueError("Fix.setAriesFile: The file violates the parameter rules")                    
+            else:
+                raise ValueError("Fix.setAriesFile: The file violates the parameter rules")                    
+            
+            try:
+                self.ariesFile = ariesFile
+                f = open(self.log, "a")
+                f.write("LOG: " + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + "-6:00\t" + "Aries file:\t" + os.path.abspath(self.ariesFile) + "\n")
+            
+                ariesDict = {}
+                with open(ariesFile) as af:
+                    for line in af:
+                        entry = line.split("\t", 1)
+                        date = entry[0]
+                        values = entry[1].split()
+                        if date in ariesDict:
+                            ariesDict[date].append(values)
+                        else:
+                            ariesDict[date] = [values]
+                        
+                self.ariesDictionary = ariesDict
+                return os.path.abspath(self.ariesFile)
+            except:    
                 raise ValueError("Fix.setAriesFile: The file violates the parameter rules")                    
         else:
-            raise ValueError("Fix.setAriesFile: The file violates the parameter rules")                    
-        
-        try:
-            self.ariesFile = ariesFile
-            f = open(self.log, "a")
-            f.write("LOG: " + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + "-6:00\t" + "Aries file:\t" + os.path.abspath(self.ariesFile) + "\n")
-        
-            ariesDict = {}
-            with open(ariesFile) as af:
-                for line in af:
-                    entry = line.split("\t", 1)
-                    date = entry[0]
-                    values = entry[1].split()
-                    if date in ariesDict:
-                        ariesDict[date].append(values)
-                    else:
-                        ariesDict[date] = [values]
-                    
-            self.ariesDictionary = ariesDict
-            return os.path.abspath(self.ariesFile)
-        except:    
-            raise ValueError("Fix.setAriesFile: The file violates the parameter rules")                    
+            raise ValueError("Fix.setAriesFile: The file violates the parameter rules")
     
-    def setStarFile(self, starFile):
+    def setStarFile(self, starFile = None):
         if(starFile == None):
             raise ValueError("Fix.setStarFile: The file violates the parameter rules 1")                    
-        
-        checkForPeriod = starFile.find(".")
-        if(checkForPeriod != -1):
-            enteredFileType = starFile[checkForPeriod + 1: len(starFile)]
-            if(enteredFileType != "txt"):
-                raise ValueError("Fix.setStarFile: The file violates the parameter rules 2")                    
+        if(isinstance(starFile, basestring)):
+            checkForPeriod = starFile.find(".")
+            if(checkForPeriod != -1):
+                enteredFileType = starFile[checkForPeriod + 1: len(starFile)]
+                if(enteredFileType != "txt"):
+                    raise ValueError("Fix.setStarFile: The file violates the parameter rules 2")                    
+            else:
+                raise ValueError("Fix.setStarFile: The file violates the parameter rules 3")                    
+            
+            try:
+                self.starFile = starFile
+                f = open(self.log, "a")
+                f.write("LOG: " + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + "-6:00\t" + "Star file:\t" + os.path.abspath(self.starFile) + "\n")
+            
+                starDict = {}
+                with open(starFile) as sf:
+                    for line in sf:
+                        entry = line.split("\t", 1)
+                        star = entry[0]
+                        values = entry[1].split()
+                        if star in starDict:
+                            starDict[star].append(values)
+                        else:
+                            starDict[star] = [values]
+                        
+                self.starDictionary = starDict
+                return os.path.abspath(self.starFile)
+            except:    
+                raise ValueError("Fix.setStarFile: The file violates the parameter rules 4")        
         else:
-            raise ValueError("Fix.setStarFile: The file violates the parameter rules 3")                    
-        
-        try:
-            self.starFile = starFile
-            f = open(self.log, "a")
-            f.write("LOG: " + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + "-6:00\t" + "Star file:\t" + os.path.abspath(self.starFile) + "\n")
-        
-            starDict = {}
-            with open(starFile) as sf:
-                for line in sf:
-                    entry = line.split("\t", 1)
-                    star = entry[0]
-                    values = entry[1].split()
-                    if star in starDict:
-                        starDict[star].append(values)
-                    else:
-                        starDict[star] = [values]
-                    
-            self.starDictionary = starDict
-            return os.path.abspath(self.starFile)
-        except:    
-            raise ValueError("Fix.setStarFile: The file violates the parameter rules 4")                    
+            raise ValueError("Fix.setStarFile: The file violates the parameter rules 4")
     
     def getBody(self, count):
         xmlDocument = minidom.parseString(self.strOfSightingFile)
@@ -129,7 +136,7 @@ class Fix():
         dateList = xmlDocument.getElementsByTagName('date')
         self.date = dateList[count].firstChild.nodeValue
         try:
-            datetime.strptime(self.dateNeededForGHA, "%m/%d/%y")
+            self.dateNeededForGHA = self.date
             datetime.strptime(self.date, "%Y-%m-%d")
             return self.date
         except:
@@ -198,7 +205,7 @@ class Fix():
         self.pressure = pressureList[count].firstChild.nodeValue
         checkForPeriod = self.pressure.find(".")
         if(checkForPeriod != -1):
-            raise ValueError("Fix.getSightings: The Pressure violates the parameter rules")
+            self.errorCount += 1
         else:
             self.intPressure = int(self.pressure)
             return self.intPressure
@@ -222,20 +229,128 @@ class Fix():
         # Use celestial body as key for the star dictionary. This will retrieve the values
         # needed for Note 4 on the requirements.
         # Use the date as key for the aries dictionary. This will retrieve the values needed
-        # Once all values are retrieved we can compute the GHA
-        bodyKey = self.body
+        # Once all values are retrieved we can compute the GHA 
+        sightingDate = self.date
+        convertedDate = self.dateNeededForGHA
+        convertedDate = datetime.strptime(convertedDate,"%Y-%m-%d").strftime("%m/%d/%y")
+        convertedDate = datetime.strptime(convertedDate,"%m/%d/%y")
+        ariesDate = datetime.strptime(sightingDate, "%Y-%m-%d").strftime("%m/%d/%y")
+        sightingBody = self.body
+        timeArray = self.time.split(":") 
+        hourNeeded = int(timeArray[0])
+        s = int(timeArray[1]) * 60 + int(timeArray[2])
+        starDictionary = self.starDictionary
+        ariesDictionary = self.ariesDictionary
+        starIndex = 0
+        geoPosLatAngle = Angle.Angle()
+        geoPosLonAngle = Angle.Angle()
+        starSarHourAngle = Angle.Angle()
+        ariesGHA2Angle = Angle.Angle()
+        ariesGHA1Angle = Angle.Angle()
+        ariesGHA = Angle.Angle()
+        
+        for star in starDictionary[sightingBody]:
+            checkDate = datetime.strptime(star[0], "%m/%d/%y")
+            if (checkDate == convertedDate):
+                observedStar = starDictionary[sightingBody][starIndex]
+                break
+            elif (checkDate > convertedDate):
+                observedStar = starDictionary[sightingBody][starIndex - 1]
+                break
+        
+
+        
+        geographicPositionLatitude = observedStar[2]
+        geoPosLatAngle.setDegreesAndMinutes(geographicPositionLatitude)
+        starSHA = observedStar[1]
+        returnedSHA = observedStar[2]
+        returnedSHAANlge = Angle.Angle()
+        returnedSHAANlge.setDegreesAndMinutes(returnedSHA)
+        starSarHourAngle.setDegreesAndMinutes(starSHA)
+        
+        dateList = ariesDictionary[ariesDate]
+        aries1GHA = dateList[hourNeeded][1]
+        
+        if (hourNeeded == 23):
+            ariesDate = datetime.strptime(ariesDate, "%m/%d/%y")
+            ariesDate += timedelta(days=1)
+            ariesDate = datetime.strftime(ariesDate, "%m/%d/%y")
+            dateList = ariesDictionary[ariesDate]
+            aries2GHA = dateList[0][1]
+        else:
+            aries2GHA = dateList[hourNeeded + 1][1]
+
+        ariesGHA1Angle.setDegreesAndMinutes(aries1GHA)
+        ariesGHA2Angle.setDegreesAndMinutes(aries2GHA)
+        part1 = float(ariesGHA1Angle.angle)
+        part2 = float(ariesGHA2Angle.angle)
+
+        computeFormula = abs(part2 - part1)
+        computeFormula = float(computeFormula) * s/3600
+        ariesGHA.setDegrees(computeFormula)
+        ariesGHA.add(ariesGHA1Angle)
+        
+        ariesGHA.add(starSarHourAngle)
+        
+        
+        
+        geoPosLonAngle.setDegrees((ariesGHA.angle) % 360)
+        self.latitude = returnedSHAANlge.getDegrees()
+        self.longitude = geoPosLonAngle.getDegrees()
+        return (returnedSHA, geoPosLonAngle)
         
             
-    def getSightings(self):
+    def getSightings(self, assumedLatitude = "0d0.0", assumedLongitude = "0d0.0"):
         if(self.strOfSightingFile == None):
             raise ValueError("Fix.getSightings: Sighting file not set")
         if(self.ariesDictionary == None):
             raise ValueError("Fix.getSightings: Aries file not set")
         if(self.starDictionary == None):
             raise ValueError("Fix.getSightings: Star file not set")
+        if(not(isinstance(assumedLatitude, basestring))):
+            raise ValueError("Fix.getSightings: assumedLatitude not string")
+        if(not(isinstance(assumedLongitude, basestring))):
+            raise ValueError("Fix.getSightings: assumedLongitude not string")
+        try:
+            checkStrLat = assumedLatitude.split("d")
+            if (float(checkStrLat[1]) < 0.0):
+                raise ValueError("Fix.getSightings: The parameters are illegal")
+    
+            decimalCheck = abs(Decimal(checkStrLat[1]).as_tuple().exponent)
+            if (decimalCheck > 1):
+                raise ValueError("Fix.getSightings: The parameters are illegal")
+            
+            checkStrLon = assumedLongitude.split("d")
+            if (float(checkStrLon[1]) < 0.0):
+                raise ValueError("Fix.getSightings: The parameters are illegal")
+    
+            decimalCheck = abs(Decimal(checkStrLon[1]).as_tuple().exponent)
+            if (decimalCheck > 1):
+                raise ValueError("Fix.getSightings: The parameters are illegal")
+        except:
+            raise ValueError("Fix.getSightings: The parameters are illegal")
+        
+        checkForLatN = assumedLatitude.find("N")
+        checkForLatS = assumedLatitude.find("S")
+        if (checkForLatN == -1):
+            if (checkForLatS == -1):
+                assumedLatitude = "0d0.0"
+                print assumedLatitude
+            else:
+                assumedLatitude = "-" + assumedLatitude[1:]
+                print assumedLatitude
+        else:
+            assumedLatitude = assumedLatitude[1:]
+            print assumedLatitude
+            
         
         self.errorCount = 0
         self.totalErrorCount = 0
+        self.sumCosAzimuth = 0
+        self.sumSinAzimuth = 0
+        self.sumDistance = 0
+        self.multCosVar = 0
+        self.multSinVar = 0
         xmlDocument = minidom.parseString(self.strOfSightingFile)
         sightingList = xmlDocument.getElementsByTagName('sighting')
         amount = len(sightingList)
@@ -251,26 +366,53 @@ class Fix():
             self.getPressure(i)
             self.getTemp(i)
             self.getTime(i)
-            usedAngle.setDegreesAndMinutes(self.observation)
-            altitude = usedAngle.getDegrees()
-            if(self.horizon == "Natural"):
-                self.dip = (-0.97 * sqrt(float(self.height))) / 60
-            else:
-                self.dip = 0
-            self.refraction = (-0.00452 * self.intPressure) / (273 + ((5/9) * (self.intTemperature - 32))) / tan(altitude)
-            self.adjustedAltitude = (tan(altitude)) + self.dip + self.refraction
+            observedAltitude = usedAngle.setDegreesAndMinutes(self.observation)
             if(self.errorCount == 0):
                 #self.getGHA()
-                f.write("LOG: " + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + "-6:00\t" + self.body + "\t" + self.date + "\t" + self.time + "\t" + self.observation + "\n")
+                if(self.horizon.lower() == "natural"):
+                    self.dip = float((-0.97 * sqrt(float(self.numericHeight))) / 60.0)
+                else:
+                    self.dip = 0.0
+                self.refraction = (-0.00452 * self.intPressure) / (273 + (.5556 * (self.intTemperature - 32))) / tan(observedAltitude * pi/180)
+                self.adjustedAltitude = round(observedAltitude + self.dip + self.refraction, 3)
+                usedAngle.angle = self.adjustedAltitude
+                (lat, lon) = self.getGHA()
+
+                assumedLonAngle = Angle.Angle()
+                assumedLatAngle = Angle.Angle()
+                assumedLatAngle.setDegreesAndMinutes(assumedLatitude)
+                assumedLonAngle.setDegreesAndMinutes(assumedLongitude)
+                
+                LHA = self.longitude - assumedLonAngle.getDegrees()
+                
+                correctedAltitude = math.asin((math.sin(self.latitude) * math.sin(assumedLatAngle.getDegrees()))) + (math.cos(self.latitude) * math.cos(assumedLatAngle.getDegrees()) * math.cos(LHA))
+                distanceAdjustment = round(usedAngle.getDegrees() - correctedAltitude, 1)
+                azimuthAdjustment = math.acos(math.sin(self.latitude) - math.sin(assumedLatAngle.getDegrees()) * (math.cos(assumedLatAngle.getDegrees()) * math.cos(distanceAdjustment)))
+                self.sumDistance += distanceAdjustment
+                self.sumCosAzimuth += math.cos(azimuthAdjustment)
+                self.sumSinAzimuth += math.sin(azimuthAdjustment)
+                self.multCosVar += self.sumDistance * self.sumCosAzimuth 
+                self.multSinVar += self.sumDistance * self.sumSinAzimuth
+                
+                f.write("LOG: " + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + "-6:00\t" + self.body + "\t" + self.date + "\t" + self.time + "\t" + usedAngle.getString() + "\t" + str(lat) + "\t" + str(lon.getString()) + "\t" + str(assumedLatitude) + "\t" + str(assumedLongitude) + "\t" + str(azimuthAdjustment) + "\t" + str(round(distanceAdjustment), 1) +"\n")
+
             else:
                 self.totalErrorCount += 1
                 self.errorCount = 0
-                continue    
             
-        f.write("LOG: " + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + "-6:00\t" + "Sighting errors:\t" + str(self.totalErrorCount))
+        f.write("LOG: " + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + "-6:00\t" + "Sighting errors:\t" + str(self.totalErrorCount) + "\n")
+        
+        latDegrees = assumedLatAngle.getDegrees()    
+        degreesLatitude = latDegrees + (self.multCosVar / 60)
+        assumedLatAngle.setDegrees(degreesLatitude)
+        approximateLatitude = assumedLatAngle.getString()
+        
+        lonDegrees = assumedLonAngle.getDegrees()    
+        degreesLongitude = lonDegrees + (self.multSinVar / 60)
+        assumedLonAngle.setDegrees(degreesLongitude)
+        approximateLongitude = assumedLonAngle.getString()
+        
+        f.write("LOG: " + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") + "-6:00\t" + "Approximate latitude:\t" + approximateLatitude + "\t" + "Approximate Longitude:\t" + approximateLongitude)
+        return (approximateLatitude, approximateLongitude)
         f.close()
-        appLatitude = "0d0.0"
-        appLongitude = "0d0.0"
-        return (appLatitude, appLongitude)
-  
     
